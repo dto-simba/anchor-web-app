9b02381b569295abb31d81bfd0d3f220cd081e95
Carson <779835174@qq.com>
2023-02-06 18:15:51 +0800

    update

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -8,6 +8,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
+    // alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
     alias: {
       //'@terra-money/terra.js': '@terra-money/terra.js/dist/bundle.js',
       'styled-components':
